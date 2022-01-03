500ef526d20a08552e10cae538c281c9cd01cc63
Oleja-mo <95916811+Oleja-mo@users.noreply.github.com>
2022-01-03 09:51:09 +0500

    Олег добавил свои классы (поле и пуля)

--- main.py
+++ main.py
@@ -1,112 +1,59 @@
-from PIL import Image
-import os
-import sys
 import pygame
+import pytmx
+
+WIND_SIZE = WIND_HEIGHT, WIND_WIDTH = 1600, 900
+FPS = 60
+MAPS_DIR = 'тайлы'
+TILE_SIZE = 64
 
 
-def load_image(name, colorkey=None):
-    fullname = os.path.join('images', name)
-    # если файл не существует, то выходим
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
+class Room:
+    def __init__(self, filename):
+        self.map = pytmx.load_pygame(f"{MAPS_DIR}/{filename}")
+        self.height = self.map.height
+        self.width = self.map.width
+        self.tile_size = self.map.tilewidth
 
-class Weapon:
-    def __init__(self, screen, x, y, sprite, shooting_sprite): # + sprite
-        self.x, self.y = x, y
-        self.spr = sprite
-        self.shoot_spr = shooting_sprite
-        self.V = [0, 0]  # y x
+    def render(self, screen, pos):
+        for y in range(self.height):
+            for x in range(self.width):
+                image = self.map.get_tile_image(x, y, 0)
+                screen.blit(image, (x * self.tile_size - pos[0], y * self.tile_size - pos[1]))
 
-class Entity:
-    def __init__(self, screen, x, y, max_health, sprite): # + sprite
-        self.max_health, self.health = max_health, max_health
-        self.x, self.y = x, y
-        self.spr = sprite
-        self.V = [0, 0]  # y x
-
-    def move(self):
-        global t, screen
-        screen.fill((0, 0, 0))
-        x = self.spr.rect.x
-        y = self.spr.rect.y
-        x_pos, y_pos = (x + self.V[1] * t / 100), (y + self.V[0] * t / 100)
-        self.spr.rect.x = x_pos
-        self.spr.rect.y = y_pos
-        #all_sprites.draw(screen)
-
-    def change_v(self, yv, xv):
-        if yv and not xv:
-            self.V = [yv[0], self.V[1]]
-        elif xv and not yv:
-            self.V = [self.V[0], xv[0]]
-
-    def get_v(self):
-        return self.V
-
-class Hero(Entity):
-    pass
+    def get_tile_id(self, pos):
+        return self.map.tiledgidmap[self.map.get_tile_gid(*pos, 0)] - 1
 
 
-
-if __name__ == '__main__':
+def main():
     pygame.init()
-    pygame.display.set_caption('игра')
-    size = width, height = 400, 400
-    screen = pygame.display.set_mode(size)
-    fps = 60
-    #Ve = [0, 0, 0, 0]  # w s a d
+    screen = pygame.display.set_mode(WIND_SIZE)
+
+    room = Room('1.tmx')
+    pos = [0, 0]
+
     clock = pygame.time.Clock()
-    f = False
-
-    all_sprites = pygame.sprite.Group()
-    hero_sprite = pygame.sprite.Sprite()
-    hero_sprite.image = load_image("mouse.png", colorkey=-1)
-    hero_sprite.rect = hero_sprite.image.get_rect()
-    hero_sprite.rect.x = 5
-    hero_sprite.rect.y = 20
-    all_sprites.add(hero_sprite)
-    all_sprites.draw(screen)
-
-    hero = Hero(hero_sprite)
-
     running = True
+    gameover = False
+
     while running:
-        pygame.display.flip()
-        t = clock.tick()
-        hero.move()
-        clock.tick(fps)
-        all_sprites.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN:
 
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_w:
-                hero.change_v([-200], [])
-            if event.type == pygame.KEYUP and event.key == pygame.K_w:
-                hero.change_v([0], [])
+                if event.key == 119:
+                    pos[1] -= 10
+                elif event.key == 100:
+                    pos[0] += 10
+                elif event.key == 115:
+                    pos[1] += 10
+                elif event.key == 97:
+                    pos[0] -= 10
+        screen.fill((8, 10, 10))
+        room.render(screen, pos)
+        pygame.draw.circle(screen, (255, 0, 0), (800, 450), 5)
+        pygame.display.flip()
+        clock.tick(FPS)
 
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_s:
-                hero.change_v([200], [])
-            if event.type == pygame.KEYUP and event.key == pygame.K_s:
-                hero.change_v([0], [])
 
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_a:
-                hero.change_v([], [-200])
-            if event.type == pygame.KEYUP and event.key == pygame.K_a:
-                hero.change_v([], [0])
-
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
-                hero.change_v([], [200])
-            if event.type == pygame.KEYUP and event.key == pygame.K_d:
-                hero.change_v([], [0])
-    pygame.quit()
+main()
